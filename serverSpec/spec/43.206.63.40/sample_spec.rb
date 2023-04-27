13bde14094135ac97e37f9725bbb120774657cbb
akihisa <112173273+akitoc342@users.noreply.github.com>
2023-04-27 22:49:16 +0000

    serverspec確認項目追加

--- serverSpec/spec/43.206.63.40/sample_spec.rb
+++ serverSpec/spec/43.206.63.40/sample_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
+listen_port = 80
+
+#gitインストール確認
 describe package('git') do
   it { should be_installed }
+end
+
+#nginx起動確認
+describe service('nginx') do
+  it { should be_running }
+end
+
+#unicorn動作確認
+describe command("ps aux | grep unicorn" ) do
+  its(:stdout) { should contain("unicorn master") }
+end
+
+describe command("curl http://#{ENV['ALB_DNS']}/ -o /dev/null -w \"%{http_code}\\n\" -s") do
+its(:stdout) { should match /^200$/ }
 end
